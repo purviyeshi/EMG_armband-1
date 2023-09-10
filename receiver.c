6a0489766878ee76206f8fdf35be5277501aee1e
anuj-akotkar <142881764+anuj-akotkar@users.noreply.github.com>
2023-09-10 23:41:50 +0530

    receiver code for receive data through ESP-NOW
    LSA READING RECEIVE THROUGH ESP-NOW

--- receiver.c
+++ receiver.c
@@ -3,18 +3,27 @@
 #include "esp_wifi.h"
 #include "esp_now.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
 
 static const char *TAG = "RECEIVER";
-uint8_t receiverAddress[] = {0xE8, 0x31, 0xCD, 0xC3, 0xB0, 0x00}; // Replace with the MAC address of this ESP   
+uint8_t receiverAddress[] = {0xC4, 0xDD, 0x57, 0x5B, 0xF8, 0x54}; // Replace with the MAC address of this ESP   
 char receivedMessage[100];
 void ondatareceived(const uint8_t *mac_addr, const uint8_t *data, int len) {
     ESP_LOGI(TAG,"Received message: %.*s\n", len, data);
-    int data_len = sizeof(receivedMessage) / sizeof(receivedMessage[0]);
-    memcpy(receivedMessage,data,data_len);
-    receivedMessage[data_len];  
+    //int data_len = sizeof(receivedMessage) / sizeof(receivedMessage[0]);
+    //memcpy(receivedMessage,data,data_len);
+    //receivedMessage[data_len];  
 }
 
 void app_main(void) {
+
+//initialize nvs_flash
+    esp_err_t cpy = nvs_flash_init();
+    if (cpy == ESP_ERR_NVS_NO_FREE_PAGES || cpy == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        cpy = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(cpy);
    
 
     // Initialize WiFi
@@ -23,7 +32,6 @@ void app_main(void) {
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
-
     // Initialize ESP-NOW
      esp_err_t result;
     result = esp_now_init();
@@ -34,7 +42,7 @@ void app_main(void) {
     
     //Add senders that you expect to receive data from 
      esp_now_peer_info_t peerInfo;
-     uint8_t senderMacAddress[] = {0xE8, 0x31, 0xCD, 0xC3, 0xEF, 0x8C};
+     uint8_t senderMacAddress[] = {0xC4, 0xDD, 0x57, 0xC9, 0x8D, 0x30};       //  Transmitter MAC Address: C4:DD:57:C9:8D:30
     memset(&peerInfo, 0, sizeof(esp_now_peer_info_t));
     memcpy(peerInfo.peer_addr, senderMacAddress, 6);
     peerInfo.channel = 0;
@@ -43,6 +51,8 @@ void app_main(void) {
 
     // Register callback for receiving data
     esp_now_register_recv_cb(ondatareceived);
+    
+      
 
      while (1) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
